bb5e9f25c764dbc48a627074515a96c58a1a6eeb
laras126 <laras126@gmail.com>
2020-10-01 10:38:10 -0400

    chore: whitespace, rm unused files

--- src/formats/report.js
+++ src/formats/report.js
@@ -9,16 +9,16 @@ const { getArgFromCLI } = require( '../utils/cli' );
 
 /**
  * Get the source of a template to compile.
- * 
- * @param {string} subdir 
- * @param {string} name 
- * 
+ *
+ * @param {string} subdir
+ * @param {string} name
+ *
  * @returns {string} Template contnet
  */
 const getTemplateSrc = ( name ) => {
 	const templatePath = ( name ) => path.join( __dirname, `./templates/${name}.twig` );
 	const fileName = fs.existsSync( templatePath( name ) ) ? name : 'base';
-	
+
 	return fileName + '.twig';
 };
 
@@ -26,7 +26,7 @@ let loader = new TwingLoaderFilesystem( path.join( __dirname, './templates' ) );
 let twing = new TwingEnvironment( loader, { debug: true } );
 
 module.exports = module.exports = function( reports ) {
-	
+
 	const reportName = getArgFromCLI('--report');
 	const reportDest = path.join( __dirname, `../../public/${reportName}.html` );
 	const reportTemplate = getTemplateSrc( reportName );
@@ -34,13 +34,13 @@ module.exports = module.exports = function( reports ) {
 		reports,
 		title: `CSS Audit for ${reportName}`
 	};
-	
+
 	twing.render( reportTemplate, context ).then( ( output ) => {
 		console.log( `Generated template for ${reportTemplate}.` );
 		fs.writeFileSync( reportDest, output );
 	}).catch( ( e ) => {
 		console.error( e );
 	});
-	
+
 };
 
