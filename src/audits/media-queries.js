2093185c7655378ffb98ab3a67a8427a0f899029
laras126 <laras126@gmail.com>
2020-10-29 13:30:36 -0700

    fix: add audit key

--- src/audits/media-queries.js
+++ src/audits/media-queries.js
@@ -52,36 +52,43 @@ module.exports = function ( files = [] ) {
 	return [
 		{
 			id: 'count',
+			audit: 'media-queries',
 			label: 'Number of total media queries',
 			value: allQueries.length,
 		},
 		{
 			id: 'count-unique-queries',
+			audit: 'media-queries',
 			label: 'Number of seemingly-unique media queries',
 			value: uniqQueries.length,
 		},
 		{
 			id: 'top-10-queries',
+			audit: 'media-queries',
 			label: 'Top 10 most-used media queries',
 			value: queriesByCount.slice( 0, 10 ),
 		},
 		{
 			id: 'count-unique-sizes',
+			audit: 'media-queries',
 			label: 'Number of unique breakpoint sizes',
 			value: uniqSizes.length,
 		},
 		{
 			id: 'top-10-sizes',
+			audit: 'media-queries',
 			label: 'Top 10 most-used breakpoint sizes',
 			value: sizesByCount.slice( 0, 10 ),
 		},
 		{
 			id: 'bottom-10-sizes',
+			audit: 'media-queries',
 			label: 'Top 10 least-used breakpoint sizes',
 			value: sizesByCount.slice( -10 ).reverse(),
 		},
 		{
 			id: 'non-width',
+			audit: 'media-queries',
 			label: 'Non-width related media queries',
 			value: nonWidthByCount,
 		},
