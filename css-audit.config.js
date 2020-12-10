d9131df457eb5ce564ec8d5c51c8e8311b8bcc6d
laras126 <laras126@gmail.com>
2020-12-10 15:52:59 -0500

    fix: update config

--- css-audit.config.js
+++ css-audit.config.js
@@ -5,7 +5,7 @@ module.exports = {
 	filename: 'wp-admin',
 	all: true,
 	audits: [
-		[ 'property-values', 'font-size' ],
+		[ 'property-values', 'font-size,font-family,font-weight' ],
 		[ 'property-values', 'padding-top,padding-bottom,padding-left,padding-right' ],
 	],
 };
