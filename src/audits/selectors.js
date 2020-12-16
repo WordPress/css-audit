d4e3d66865182ff118d0369fa73b6a412efb7fc7
Dan Farrow <dan@squarebracket.net>
2020-12-16 01:04:36 +0000

    Update report styles (#15)
    * feat: Overhaul report styling
    
    * chore: rebuild report
    
    * feat: Style all integer values with <span class='count'>
    
    * chore: rebuild report
    
    * feat: Add css rule for smooth scrolling including prefers-reduced-motion check
    
    * feat: Add darkmode for users with 'prefers-color-scheme: dark'
    
    * feat: Add darkmode switcher
    
    * chore: rebuild report
    
    * Reverse darkmode switch icons to indicate current darkmode state
    
    * Use a button for Dark Mode toggle
    
    This will allow for better keyboard support
    
    # Conflicts:
    #	public/style.css
    #	src/formats/templates/nav.twig
    #	src/formats/templates/report.twig
    
    * Remove duplicate & unused report templates
    
    # Conflicts:
    #	src/formats/templates/audits/selectors.twig
    
    * Update audit
    
    Co-authored-by: Dan Farrow <dan@farrow.email>
    Co-authored-by: Kelly Dwan <ryelle@users.noreply.github.com>

--- src/audits/selectors.js
+++ src/audits/selectors.js
@@ -42,7 +42,6 @@ module.exports = function ( files = [] ) {
 	return {
 		audit: 'selectors',
 		name: 'Selectors',
-		template: 'selectors',
 		results: [
 			{
 				// This is not totally accurate, since nested `:not` selectors and keyframes are also counted.
